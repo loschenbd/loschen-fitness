915464127b34daac7d0ddc2b4490be5ae846e1ed
Benjamin Loschen <loschenbd@gmail.com>
2022-03-27 12:27:18 -0500

    intiial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Airtable from 'airtable';
+import {useEffect} from "react";
+
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_KEY }).base(process.env.REACT_APP_AIRTABLE_BASE
+);
+
 
 function App() {
+  useEffect(() => {
+    base('Table 1')
+        .select({view: "Grid view"})
+        .eachPage((records, fetchNextPage) => {
+          console.log(records);
+          fetchNextPage();
+        });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
