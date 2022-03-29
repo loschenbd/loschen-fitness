82c7b79f4bddc02757727ffbfdf4d6111f2e6ed2
Benjamin Loschen <loschenbd@gmail.com>
2022-03-29 07:04:26 -0500

    Get Initial Records

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ import Exercise from "./components/Exercise";
 const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_KEY }).base(process.env.REACT_APP_AIRTABLE_BASE
 );
 
-
 function App() {
+    useEffect(() => {
+        base('Exercises').select({
+            // Selecting the first 3 records in Grid view:
+            view: "Grid view"
+        }).eachPage(function page(records, fetchNextPage) {
+            // This function (`page`) will get called for each page of records.
+
+            records.forEach(function(record) {
+                console.log('Retrieved', record.get('Name'));
+            });
+
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            fetchNextPage();
+
+        }, function done(err) {
+            if (err) { console.error(err); return; }
+        });
+    }, []);
   return (
     <div className="App">
           <h1>Exercises</h1>
